05922953055059a7be14b1652e625e76e5ef238d
Khalidoffcial <kk.7halid300300@gmail.com>
2025-05-22 10:18:09 +0300

    this 6v

--- src/compontants/articlereading.jsx
+++ src/compontants/articlereading.jsx
@@ -3,36 +3,45 @@ import {useParams} from "react-router-dom";
 import { database } from './firebase';
 import { ref, onValue } from "firebase/database";
 import Top from './top';
-import { ref as storageRef, uploadBytes, getDownloadURL,deleteObject } from 'firebase/storage';
-import { storage } from './firebase'; // استيراد إعدادات Firebase
+import { ref as storageRef, getDownloadURL } from 'firebase/storage';
+import { storage } from './firebase.js'; // استيراد إعدادات Firebase
 import 'react-quill/dist/quill.snow.css'; // تضمين تنسيقات Quill
 
 const Articlereading = () => {
   const { articleId } = useParams();
-  const [content, setContent] = useState('');
   const [DataArticle, SetdataArticle] = useState([]);
   const [imageUrl, setImageUrl] = useState('');
-useEffect(()=>{
-const artcleref = ref(database, `articles/article${articleId}`);
+  useEffect(() => {
+    if (!articleId) {
+      console.error("articleId غير معرف");
+      return;
+    }
+  
+    const artcleref = ref(database, `articles/article${articleId}`);
     onValue(artcleref, (snapshot) => {
       const data = snapshot.val();
-      console.log(data);
       if (data) {
         SetdataArticle(data);
       }
-   });
-   const fetchImageUrl = async () => {
-    const imageRef = storageRef(storage, `images/image${articleId}`);
-    try {
-        const url = await getDownloadURL(imageRef);
-        setImageUrl(url);
-    } catch (error) {
-        console.error('Error fetching image URL:', error);
-    }
-};
-
-fetchImageUrl();
-},[])
+    });
+  
+    const fetchImageUrl = async (artid) => {
+      if(artid){
+        console.log(artid);
+        
+        const imgRef = storageRef(storage, `images/image${artid}`);
+        try {
+          const url = await getDownloadURL(imgRef);
+          console.log(url);
+          setImageUrl(url);
+        } catch (error) {
+          console.error('Error fetching image URL:', error);
+        }
+      }
+    };
+  
+    fetchImageUrl(articleId);
+  }, [articleId]);
 
 
 
@@ -42,7 +51,7 @@ fetchImageUrl();
     </div>
       <div className="article_information">
         <div className="img_article">
-            <img src={imageUrl} alt=""/>
+            {imageUrl? (<img src={imageUrl} alt=""/>): (null)}
             </div>
         <div className='title'>{DataArticle.title} </div>
         <div className='descrip'>{DataArticle.descrip}
